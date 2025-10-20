a11948349c6fa6f4941ea3cdf572007843ff927e
yerling sandino <yerlingsandino22@gmail.com>
2025-10-20 11:19:49 -0600

    se agrego la funcionalidad de leer el archivo una vez encontrado

--- practica.py
+++ practica.py
@@ -3,6 +3,16 @@ import sys
 script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 file_path = os.path.join(script_dir, 'planta.csv')
 
-  
+     
+try:
+    with open(file_path, 'r') as file:
+        content = file.readlines()
+        for linea in content:
+             linea_sin_puntuacion = linea.replace(',', '  ')
+             print(linea_sin_puntuacion.strip(" "))
+except FileNotFoundError:
+    print(f"Error: El archivo no se encontró en la ruta: {file_path}")
+    
+
 
     
